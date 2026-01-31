16aee04469a8a516222d3f9b266f0dccd20299b0
Craig Wechtler <30483550+cwechtler@users.noreply.github.com>
2026-01-31 10:39:46 -0600

    Attack system

---
+++ Assets/Scripts/Skills/ConeSkill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeSkill : SkillConfig
+{
+    [Header("Cone Attack Settings")]
+    [SerializeField] private float radius = 1.5f;
+    [SerializeField] private float angle = 45f;   // half‑angle of the cone
+    [SerializeField] private bool useAnimationHitTiming = false;
+
+    public float Radius => radius;
+    public float Angle => angle;
+    public bool UseAnimationHitTiming => useAnimationHitTiming;
+    public float HitDelay = 0.12f;
+}
---
+++ Assets/Scripts/Skills/ExpandingSkill.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandingSkill : SkillConfig
+{
+    [Header("Expanding AoE Settings")]
+    [SerializeField] private float startRadius = 0.1f;
+    [SerializeField] private float maxRadius = 4f;
+    [SerializeField] private float expandSpeed = 5f;
+
+    public float StartRadius => startRadius;
+    public float MaxRadius => maxRadius;
+    public float ExpandSpeed => expandSpeed;
+}
